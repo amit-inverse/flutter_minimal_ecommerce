4748c47f526915a324a447ee417ad1816c5d334d
Amit Kumar Das <amit.inverse@gmail.com>
2024-03-08 08:41:51 +0530

    010

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:minimal_ecommerce/components/my_drawer.dart';
+import 'package:provider/provider.dart';
+
+import '../components/my_drawer.dart';
+import '../models/product.dart';
+import '../models/shop.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
+  // remove item from cart method
+  void removeItemFromCart(BuildContext context, Product product) {
+    // show a dialog box to ask user to confirm to remove from cart
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: const Text('Remove this item from your cart?'),
+        actions: [
+          // cancel button
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Cancel'),
+          ),
+
+          // yes button
+          MaterialButton(
+            onPressed: () {
+              // pop dialog box
+              Navigator.pop(context);
+
+              // add to cart
+              context.read<Shop>().removeFromCart(product);
+            },
+            child: const Text('Yes'),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    // get access to the cart
+    final cart = context.watch<Shop>().cart;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -15,6 +52,32 @@ class CartPage extends StatelessWidget {
       ),
       drawer: const MyDrawer(),
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          // cart list
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.length,
+              itemBuilder: (context, index) {
+                // get individual items in cart
+                final item = cart[index];
+
+                // return as a cart tile UI
+                return ListTile(
+                  title: Text(item.name),
+                  subtitle: Text(item.price.toStringAsFixed(2)),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.remove),
+                    onPressed: () => removeItemFromCart(context, item),
+                  ),
+                );
+              },
+            ),
+          ),
+
+          // pay button
+        ],
+      ),
     );
   }
 }
